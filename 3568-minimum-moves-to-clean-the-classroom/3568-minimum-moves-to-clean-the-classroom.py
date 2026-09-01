a224d7f58f15791af3a17c81b543dff99f287944
Prabhat Tripathi <prabhattripathi9351@gmail.com>
2026-09-02 00:30:53 +0530

    Time: 1531 ms (89.47%), Space: 24.7 MB (91.23%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.py
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.py
@@ -1,17 +1,13 @@
 from typing import List
 from collections import deque
-
 class Solution:
     def minMoves(self, classroom: List[str], energy: int) -> int:
         m = len(classroom)
         n = len(classroom[0])
-
         id = [[-1] * n for _ in range(m)]
-
         k = 0
         sr = 0
         sc = 0
-
         for r in range(m):
             for c in range(n):
                 if classroom[r][c] == 'S':
@@ -20,12 +16,10 @@ class Solution:
                 elif classroom[r][c] == 'L':
                     id[r][c] = k
                     k += 1
-
         if k == 0:
             return 0
 
         total_mask = (1 << k) - 1
-
         best = [
             [
                 [-1] * (1 << k)
@@ -33,12 +27,9 @@ class Solution:
             ]
             for _ in range(m)
         ]
-
         queue = deque()
-
         best[sr][sc][0] = energy
         queue.append((sr, sc, 0, energy, 0))
-
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         while queue:
             r, c, mask, e, moves = queue.popleft()
